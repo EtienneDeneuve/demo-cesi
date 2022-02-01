bfa821346d2b652df11e75e058e4c8c3c380b5b6
Etienne Deneuve - Simplifi'ED <etienne@simplified.fr>
2022-02-01 10:45:44 +0100

    add subnet and vnet

--- main.tf
+++ main.tf
@@ -4,10 +4,20 @@ resource "azurerm_resource_group" "rg" {
   location = "West Europe"
 }
 
-
-
 # un vm
+
 # une carte reseau
+resource "azurerm_network_interface" "nic" {
+  name                = format("%s-nic", var.naming)
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.back.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
 # d'un disque eventuel
 
 # ip public
